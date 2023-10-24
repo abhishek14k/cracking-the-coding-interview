dc3d8abe76a29f2a699d02743f44aeb9c125544b
Abhishek R <37078073+abhishek14k@users.noreply.github.com>
2023-10-24 23:10:07 +0530

    1.6_StringCompression
    1.6_StringCompression

--- arrays-and-strings/1.6_StringCompression/StringCompression.java
+++ arrays-and-strings/1.6_StringCompression/StringCompression.java
@@ -13,6 +13,7 @@ public class StringCompression {
         int repeatCount = 0;
         for (int i = 0; i < str.length(); i++) {
             repeatCount++;
+			/*If next character is not similar to the current then append the current character along with the count to result*/
             if(str.length() <= i+1 || str.charAt(i) != str.charAt(i+1)) {
                 sbCompressedString.append(str.charAt(i));
                 sbCompressedString.append(repeatCount);
