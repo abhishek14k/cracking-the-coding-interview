c6ec69a79c28e7e0459f063432654bd5e19c2619
Abhishek R <37078073+abhishek14k@users.noreply.github.com>
2023-11-02 23:52:58 +0530

    2.5_Sum_Lists
    2.5_Sum_Lists

---
+++ linked-list/2.5_Sum_Lists/Sum_Lists.java
@@ -0,0 +1,17 @@
+
+public class Sum_Lists {
+
+    public static class Node {
+        public int data;
+        public Partition_LinkedList.Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
