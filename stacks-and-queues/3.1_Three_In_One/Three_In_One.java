704770e55ce595b89ff5aa81cdb2d9e3320dd354
Abhishek R <37078073+abhishek14k@users.noreply.github.com>
2023-11-12 22:48:06 +0530

    3.1_Three_In_One
    3.1_Three_In_One initial commit

---
+++ stacks-and-queues/3.1_Three_In_One/Three_In_One.java
@@ -0,0 +1,44 @@
+/*
+Problem Statement:
+Three in One: Describe how you could use a single array to implement three stacks.
+*/
+
+public class Three_In_One {
+
+    public static class ThreeStackArray {
+        private int numStack = 3;
+        private int stackSize;
+        private int[] values;   //stores the elements of all 3 stacks
+        private int[] sizes;    //keeps track of the number of values in each stack
+
+        public ThreeStackArray(int stackSize) {
+            this.stackSize = stackSize;
+            values = new int [stackSize * numStack];
+            sizes = new int[numStack];
+        }
+    }
+
+    public static class EmptyStackException extends RuntimeException {
+        public EmptyStackException(String err) {
+            super(err);
+        }
+    }
+
+    public static class FullStackException extends RuntimeException {
+        public FullStackException(String err) {
+            super(err);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ThreeStackArray stack = new ThreeStackArray(5);
+//        stack.push(0,1);
+//        stack.push(0,1);
+//        stack.push(0,1);
+//        stack.push(0,1);
+//        stack.push(0,1);
+//        stack.pop(0);
+//        stack.push(0,1);
+    }
+}
