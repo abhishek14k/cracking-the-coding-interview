cf9d099d1caebeaa0bd442b58e92a9a428c12144
Abhishek R <37078073+abhishek14k@users.noreply.github.com>
2023-11-14 23:00:19 +0530

    3.1_Three_In_One
    3.1_Three_In_One

--- stacks-and-queues/3.1_Three_In_One/Three_In_One.java
+++ stacks-and-queues/3.1_Three_In_One/Three_In_One.java
@@ -16,12 +16,45 @@ public class Three_In_One {
             values = new int [stackSize * numStack];
             sizes = new int[numStack];
         }
-		
-		//Method to return top element of that stack
+
+        public void push(int stackNum, int value) throws FullStackException {
+            if(isFull(stackNum)) {
+                throw new FullStackException("The stack is full");
+            }
+            sizes[stackNum]++;
+            values[indexOfTop(stackNum)] = value;
+        }
+
+        public int pop(int stackNum){
+            if(isEmpty(stackNum)) {
+                throw new EmptyStackException("The stack is empty!");
+            }
+            int top_index = indexOfTop(stackNum);
+            int value = values[top_index];
+            sizes[stackNum]--;
+            return value;
+        }
+
+        public int peek(int numStack) {
+            if(isEmpty(numStack)) {
+                throw new EmptyStackException("The stack is empty!");
+            }
+            return values[indexOfTop(numStack)];
+        }
+
+        //Method to return top element of that stack
         private int indexOfTop(int stackNum) {
             int offset = stackNum * stackSize;
             int size = sizes[stackNum];
-            return offset - size -1;
+            return offset + size -1;
+        }
+
+        public boolean isEmpty(int stackNum) {
+            return sizes[stackNum] == 0;
+        }
+
+        public boolean isFull(int stackNum) {
+            return sizes[stackNum] == stackSize;
         }
     }
 
@@ -40,12 +73,13 @@ public class Three_In_One {
     public static void main(String[] args) {
 
         ThreeStackArray stack = new ThreeStackArray(5);
-//        stack.push(0,1);
-//        stack.push(0,1);
-//        stack.push(0,1);
-//        stack.push(0,1);
-//        stack.push(0,1);
-//        stack.pop(0);
-//        stack.push(0,1);
+        stack.push(0,1);
+        stack.push(0,1);
+        stack.push(0,1);
+        stack.push(0,1);
+        stack.push(0,1);
+        System.out.println("Stack :"+stack);
+        stack.pop(0);
+        stack.push(0,1);
     }
 }
